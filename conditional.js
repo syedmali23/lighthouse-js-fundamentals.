39aed97bcd3fb7cfbd3861b369dec17b71dc0189
syedmali23 <132015586+syedmali23@users.noreply.github.com>
2023-06-03 23:48:29 -0400

    Add files via upload

--- conditional.js
+++ conditional.js
@@ -1,7 +1,7 @@
 const raining = true;
 const cold = false;
 
-if (raining) {
+if (!raining) {
   console.log("Don't forget your umbrella!");
 }
 
@@ -12,12 +12,18 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 const temperature = 5;
 
-if (temperature < 0) {
+if (temperature < -40) {
   console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
+} else if (temperature < 40) {
   console.log("Short sleeves won't cut it!");
 } else {
   console.log("Short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
